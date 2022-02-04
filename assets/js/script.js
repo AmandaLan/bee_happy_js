8c604c7212e8496c8c9cddf18c885b0baf429550
AmandaLan <amanda.lancheong@gmail.com>
2022-02-04 12:04:21 +0100

    animation js init doesn't work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,38 @@
+//////////////////////////// BURGER MENU ///////////////////////////////////////////
 function burgerMenu() {
-    const navigation = document.querySelector(".navigation");
-    const burger = document.querySelector(".burger");
-    const bar = document.querySelector(".bar")
-    burger.addEventListener("click", () => {
-        navigation.classList.toggle("navActiv");
-        bar.classList.toggle("barActiv")
-    });
+  const navigation = document.querySelector(".navigation");
+  const burger = document.querySelector(".burger");
+  const bar = document.querySelector(".bar");
+  burger.addEventListener("click", () => {
+    navigation.classList.toggle("navActiv");
+    bar.classList.toggle("barActiv");
+  });
 }
 burgerMenu();
+//////////////////////////// BEEKEEPER ANIMATION ///////////////////////////////////////////
+
+function beekeeper() {
+  const beekeeperLogo = document.querySelector(".beekeeperLogo");
+  window.addEventListener("scroll", () => {
+    //   alert(window.scrollY);
+    beekeeperLogo.classList.add("beekeeperShow");
+  });
+}
+
+// window.addEventListener("scroll", checkBeekeeper);
+
+// function checkBeekeeper() {
+
+//   const triggerBottom = window.innerHeight / 5 * 4;
+
+//   beekeperLogo.forEach(beekeeper => {
+
+//       const beekeeperTop = beekeeper.getBoundingClientRect().top
+
+//       if (beekeeperTop < triggerBottom) {
+//           beekeeper.classList.add("beekeeperShow")
+//       } else {
+//           beekeeper.classList.remove("beekeeperShow")
+//       }
+//   })
+// }
